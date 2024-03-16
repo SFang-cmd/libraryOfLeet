86720069bff8954d5316a00ae891e18822045b48
SFang-cmd <57546442+SFang-cmd@users.noreply.github.com>
2024-03-16 17:20:10 -0400

    Solution was found. Originally, I was not using the Math.max function, but after looking at the sample solution, I changed that. Love the use of the ternary operator and was an interesting way to think about the indices as a store relative to the sum values. Hashmaps are actually so efficient

--- src/main/java/org/leet525/Problem525.java
+++ src/main/java/org/leet525/Problem525.java
@@ -1,25 +1,24 @@
 package org.leet525;
 
+import java.util.HashMap;
+
 public class Problem525 {
     public static void main(String[] args) {
-        int[] nums = new int[]{0,1};
+        int[] nums = new int[]{0,1,0,1,1,1,1,0,0,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0};
         System.out.println(findMaxLength(nums));
     }
 
     public static int findMaxLength(int[] nums) {
-        int[] pref = new int[nums.length + 1];
+        HashMap<Integer, Integer> valToInd = new HashMap<>(); // prefix sum, index
         int sum = 0;
         int maxLen = 0;
-        pref[0] = 0;
+        valToInd.put(0, -1);
         for (int i = 0; i < nums.length; i++) {
             sum = sum + (nums[i] == 1 ? 1 : -1);
-            pref[i + 1] = sum;
-        }
-        for (int j = 0; j < pref.length; j++) {
-            for (int k = j; k < pref.length; k++) {
-                if (pref[j] == pref[k] && (k - j) > maxLen) {
-                    maxLen = k - j;
-                }
+            if (valToInd.containsKey(sum)) {
+                maxLen = Math.max(maxLen, i - valToInd.get(sum)); // Taken from optimized sample code
+            } else {
+                valToInd.put(sum, i);
             }
         }
         return maxLen;
